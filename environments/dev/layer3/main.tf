4551891eee641f0e8f9f1b2ca40195bd930666e6
Trung Nguyen <nguyencraft1314@gmail.com>
2024-05-15 08:48:50 +0700

    Refactor again

--- environments/dev/layer3/main.tf
+++ environments/dev/layer3/main.tf
@@ -1,23 +1,23 @@
 module "aks" {
   source = "../../../modules/lab-aks"
 
-  for_each = try(var.layer4.aks, {})
+  for_each = try(var.layer3.aks, {})
 
   aks = each.value.aks_object
 
   vnet_subnet_id = data.azurerm_subnet.aks_subnet.id
 
-  tags = var.layer4.tag
+  tags = var.layer3.tag
 }
 
 module "acr" {
   source = "../../../modules/lab-acr"
 
-  for_each = try(var.layer4.acr, {})
+  for_each = try(var.layer3.acr, {})
 
   principal_id = module.aks[each.value.aks_module_key].kubelet_identity_id
 
   acr = each.value.acr_object
 
-  tags = var.layer4.tag
+  tags = var.layer3.tag
 }
